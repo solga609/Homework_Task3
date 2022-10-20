c460f0cb240294090164f8c174ed5ec2643cd932
Olga <olga609@mail.ru>
2022-10-20 13:37:00 +0300

    second task_lesson3

--- Task_3.py
+++ Task_3.py
@@ -21,3 +21,40 @@ Digits=[20, 86, 37, 1]
 
 print('Сумма чисел, стоящих на нечетных позициях = ', SumOfOddDigits(Digits))
 
+# 2. Напишите программу, которая найдёт произведение пар чисел списка.
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+# in
+# 4
+# out
+# [2, 5, 8, 10]
+# [20, 40]
+# in
+# 5
+# out
+# [2, 2, 4, 8, 8]
+# [16, 16, 4]
+
+
+from random import sample
+
+def list_rand_nums(count):
+    if count<0:
+        print("Введено отрицательное значение")
+        return []
+
+    list_nums = sample(range(1, count*2), count)
+    return list_nums
+
+def mul_pairs(list_nums: list):
+    res_list = []
+    len_list = len(list_nums)
+
+    for k in range(len_list // 2):
+        res_list.append(list_nums[k] * list_nums[len_list - k - 1])
+
+    if len_list % 2:
+        res_list.append(list_nums[len_list // 2])
+    return res_list
+new_list = list_rand_nums(int(input("Введите значение: ")))
+print(new_list)
+print(mul_pairs(new_list))
